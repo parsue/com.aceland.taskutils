84c1bd3c0e1cb513deda268b651624fdc1de549a
ACELANDWORKSHOP\parsu <parsue@gmail.com>
2025-10-05 13:58:14 +0800

    enhance json async function

--- Runtime/Extensions/JsonAsyncExtension.cs
+++ Runtime/Extensions/JsonAsyncExtension.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Threading;
 using System.Threading.Tasks;
 using AceLand.Library;
 using AceLand.Library.Models;
@@ -9,40 +8,53 @@ namespace AceLand.TaskUtils.Extensions
 {
     public static class JsonAsyncExtension
     {
-        public static Task<string> ToJsonAsync<T>(this T data, CancellationToken token, bool withTypeName = false)
+        public static Promise<JsonData> ToJsonAsync<T>(this T data, bool withTypeName = false)
         {
-            return Task.Factory.StartNew(() =>
-            {
-                try
+            var token = Promise.ApplicationAliveToken;
+            return Task.Run(() =>
                 {
-                    var settings = withTypeName ? ALib.JsonSerializerSettingsWithType : ALib.JsonSerializerSettings;
-                    var json = JsonConvert.SerializeObject(data, Formatting.None, settings);
-                    return json;
-                }
-                catch (Exception e)
-                {
-                    return token.IsCancellationRequested
-                        ? null
-                        : throw new Exception($"Serialize {typeof(T).Name} to json error\n{e}");
-                }
-            }, token, TaskCreationOptions.LongRunning, TaskScheduler.Default);
+                    try
+                    {
+                        var settings = withTypeName
+                            ? ALib.JsonSerializerSettingsWithType
+                            : ALib.JsonSerializerSettings;
+                        var json = JsonConvert.SerializeObject(data, Formatting.None, settings);
+                        var builder = JsonData.Builder().WithText(json);
+                        return withTypeName
+                            ? builder.WithTypeName().Build()
+                            : builder.Build();
+                    }
+                    catch (Exception e)
+                    {
+                        return token.IsCancellationRequested
+                            ? null
+                            : throw new Exception($"Serialize {typeof(T).Name} to json error\n{e}");
+                    }
+                },
+                token
+            );
         }
 
-        public static Task<T> ToDataAsync<T>(this JsonData jsonData, CancellationToken token, bool withTypeName = false)
+        public static Promise<T> ToDataAsync<T>(this JsonData jsonData)
         {
-            return Task.Factory.StartNew(() =>
-            {
-                try
+            var token = Promise.ApplicationAliveToken;
+            return Task.Run(() =>
                 {
-                    var settings = jsonData.WithTypeName ? ALib.JsonSerializerSettingsWithType : ALib.JsonSerializerSettings;
-                    var data = JsonConvert.DeserializeObject<T>(jsonData.Text, settings);
-                    return data;
-                }
-                catch (Exception e)
-                {
-                    throw new Exception($"Deserialize json to {typeof(T).Name} error\n{e}");
-                }
-            }, token, TaskCreationOptions.LongRunning, TaskScheduler.Default);
+                    try
+                    {
+                        var settings = jsonData.WithTypeName
+                            ? ALib.JsonSerializerSettingsWithType
+                            : ALib.JsonSerializerSettings;
+                        var data = JsonConvert.DeserializeObject<T>(jsonData.Text, settings);
+                        return data;
+                    }
+                    catch (Exception e)
+                    {
+                        throw new Exception($"Deserialize json to {typeof(T).Name} error\n{e}");
+                    }
+                },
+                token
+            );
         }
     }
 }
